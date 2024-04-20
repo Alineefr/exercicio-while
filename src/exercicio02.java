e7ac5d731b32a221c8cc5d3f4a7f2f0146c0ecce
Aline Frescura <alinefrescura69@gmail.com>
2024-04-19 21:56:06 -0300

    primeiro commit

---
+++ src/exercicio02.java
@@ -0,0 +1,14 @@
+public class exercicio02 {
+    public static void main(String[] args) {
+        
+        /*Utilizando (DoWhile), elaborar um programa que apresente
+         o somatório dos valores pares existentes na faixa de 1 até 500. */
+
+         int contador = 1;
+         int soma = 0;
+
+         
+
+
+    }
+}
